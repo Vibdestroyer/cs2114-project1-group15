cfa9161df84ffd53d0904ac3f83358ca87b80f0a
fkwon10 <francisk5@vt.edu>
2026-09-22 17:47:27 -0400

    Updated Restaurant class
    Refactor Restaurant class to improve constructor and comments.

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,41 +1,38 @@
 import java.util.Random;
 
+/**
+ * Represents a restaurant encounter with randomized waiting times.
+ */
 public class Restaurant extends Encounter {
     private boolean takesOnlineOrders;
     private boolean grabAndGo;
 
     /**
-     * Creates a new Restaurant encounter with randomized service speeds
-     * and sets up the corresponding choice.
+     * Creates a new Restaurant encounter.
      */
     public Restaurant() {
-        super("You pass by a campus dining center smelling delicious food.");
+        super();
         
         Random rand = new Random();
 
-        // Randomly assigns grab and go availability and online orders.
         this.takesOnlineOrders = rand.nextBoolean();
         this.grabAndGo = rand.nextBoolean();
 
-        // Base time cost is 10 minutes.
+        // Base time cost is 10 minutes
         int timeChange = 10;
 
-        // Reduces the time cost by 4 if online orders are available.
         if (this.takesOnlineOrders) {
             timeChange -= 4;
         }
 
-        // Reduces time cost by 4 if the food can be eaten on the walk.
         if (this.grabAndGo) {
             timeChange -= 4;
         }
         
         int distanceChange = 3;
 
-        // Result text displayed after the player picks this option
         String resultText = "You grab your food and continue rushing to class.";
 
-        // Construct Choice with all 4 parameters: menuText, resultText, timeCost, distanceReduction
         Choice getFood = new Choice(
             "Get food from restaurant",
             resultText,
@@ -43,7 +40,7 @@ public class Restaurant extends Encounter {
             distanceChange
         );
 
-        this.addChoice(getFood);
+        this.choices.add(getFood);
     }
 
     /**
