d59b72bc3b093ead77b3481f632d1cefb1ce7a2f
fkwon10 <francisk5@vt.edu>
2026-09-22 17:37:11 -0400

    Updated Restaurant class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,33 +1,62 @@
 import java.util.Random;
 
 public class Restaurant extends Encounter {
-  private boolean takesOnlineOrders;
-  private boolean grabAndGo;
+    private boolean takesOnlineOrders;
+    private boolean grabAndGo;
 
-  public Restaurant() {
-    super();
-    Random rand = new Random();
+    /**
+     * Creates a new Restaurant encounter with randomized service speeds
+     * and sets up the corresponding choice.
+     */
+    public Restaurant() {
+        super("You pass by a campus dining center smelling delicious food.");
+        
+        Random rand = new Random();
 
-    // Randomly assigns grab and go availability and online orders.
-    this.takesOnlineOrders = rand.nextBoolean();
-    this.grabAndGo = rand.nextBoolean();
+        // Randomly assigns grab and go availability and online orders.
+        this.takesOnlineOrders = rand.nextBoolean();
+        this.grabAndGo = rand.nextBoolean();
 
-    // The time cost is 10 minutes.
-    int timeChange = -10;
+        // Base time cost is 10 minutes.
+        int timeChange = 10;
 
-    // Reduces the time cost by 4 if they choose online orders.
-    if (this.takesOnlineOrders) {
-      timeChange += 4;
+        // Reduces the time cost by 4 if online orders are available.
+        if (this.takesOnlineOrders) {
+            timeChange -= 4;
+        }
+
+        // Reduces time cost by 4 if the food can be eaten on the walk.
+        if (this.grabAndGo) {
+            timeChange -= 4;
+        }
+        
+        int distanceChange = 3;
+
+        // Result text displayed after the player picks this option
+        String resultText = "You grab your food and continue rushing to class.";
+
+        // Construct Choice with all 4 parameters: menuText, resultText, timeCost, distanceReduction
+        Choice getFood = new Choice(
+            "Get food from restaurant",
+            resultText,
+            timeChange,
+            distanceChange
+        );
+
+        this.addChoice(getFood);
     }
 
-    // Reduces time cost by 4 if the food can be eaten on the walk.
-    if (this.grabAndGo) {
-      timeChange += 4;
+    /**
+     * @return Whether online orders are accepted.
+     */
+    public boolean getTakesOnlineOrders() {
+        return takesOnlineOrders;
     }
-    int distanceChange = -3;
 
-    // Add the choice to the encounter's list of choices.
-    Choice getFood = new Choice("Get food from restaurant", timeChange, distanceChange);
-    this.choices.add(getFood);
-  }
+    /**
+     * @return Whether grab-and-go is available.
+     */
+    public boolean isGrabAndGo() {
+        return grabAndGo;
+    }
 }
